ee53ea0eabc666ae2e3ce8fa83e7e34c0c03b730
TeaChareqishvili <teachareqishvili89@gmail.com>
2025-04-21 00:03:30 +0400

    finishing the favorite food context

--- components/mealApp/screens/FavoriteFood.js
+++ components/mealApp/screens/FavoriteFood.js
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { FavoriteContext } from "../../../store/context/favorite-context";
-
+import { View, Text, StyleSheet } from "react-native";
 import { MEALS } from "../../../data/dummy-data";
 
 import MealFlatList from "../meallAppComponents/MealFlatList";
@@ -10,5 +10,27 @@ export default function FavoriteFood() {
 
   const favorites = MEALS.filter((food) => favoriteFood.id.includes(food.id));
 
-  return <MealFlatList data={favorites} />;
+  return (
+    <View style={styles.mainContainer}>
+      {favorites.length == 0 ? (
+        <Text style={styles.text}>No Favorite Meals Yet.</Text>
+      ) : (
+        <MealFlatList data={favorites} />
+      )}
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    backgroundColor: "#d7e6ea",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#ae2012",
+    fontFamily: "open-sans-bold",
+    fontSize: 24,
+  },
+});
