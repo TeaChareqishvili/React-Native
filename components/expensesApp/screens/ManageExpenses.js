95cecb404d17abfb9552e03d970ffdc607d30a00
TeaChareqishvili <teachareqishvili89@gmail.com>
2025-08-21 14:22:12 +0400

    add delete button

--- components/expensesApp/screens/ManageExpenses.js
+++ components/expensesApp/screens/ManageExpenses.js
@@ -1,5 +1,7 @@
 import { useLayoutEffect } from "react";
-import { Text } from "react-native";
+import { StyleSheet, View } from "react-native";
+import IconButton from "../expensiveAppComponents/IconButton";
+import { ExpenseColors } from "../../../utils/Colors";
 
 export default function ManageExpenses({ route, navigation }) {
   const edetedExpenseId = route.params?.expenseId;
@@ -11,5 +13,35 @@ export default function ManageExpenses({ route, navigation }) {
     });
   }, [navigation, isEdeting]);
 
-  return <Text>Manage expenses</Text>;
+  function deleteExpenseHandler() {}
+  return (
+    <View style={styles.container}>
+      {isEdeting && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={ExpenseColors.error500}
+            size={34}
+            onPress={deleteExpenseHandler}
+          />
+        </View>
+      )}
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: ExpenseColors.primary700,
+  },
+
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: ExpenseColors.primary500,
+    alignItems: "center",
+  },
+});
--- utils/Colors.js
+++ utils/Colors.js
@@ -6,3 +6,18 @@ const Colors = {
 };
 
 export default Colors;
+
+export const ExpenseColors = {
+  primary50: "#BED7F3",
+  primary100: "#c6affc",
+  primary200: "#a281f0",
+  primary400: "#27374F",
+  primary500: "#415a77",
+  primary700: "#748495",
+  primary800: "#A0A4A9",
+  accent500: "#FF8AC7",
+  error50: "#fcc4e4",
+  error500: "#B11B1B",
+  gray500: "#39324a",
+  gray700: "#221c30",
+};
